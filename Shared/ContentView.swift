ed87ebca994d93c77ad47462c98eac432f7ff20c
Damiaan Dufaux <damiaan@dpro.be>
2021-06-17 19:20:22 +0200

    Add outline view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		List {
+			OutlineGroup([Item(path: [], number: 10)], children: \.children) { item in
+				Text(item.pathString)
+			}
+		}
     }
+}
+
+struct Item: Identifiable {
+	let path: [UInt8]
+	let number: UInt8
+
+	var children: [Item]? {
+		print("get children of", pathString)
+		guard number != 0 else { return nil }
+		return (0..<number).map{ Item(path: id, number: $0) }
+	}
+
+	var id: [UInt8] { path + [number] }
+	var pathString: String {
+		id.map(String.init).joined(separator: ".")
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
