a54038f66ec72a925ca7617fe43db512b489f485
Damiaan Dufaux <damiaan@dpro.be>
2021-06-17 18:41:11 +0200

    Initial Commit

---
+++ Shared/OutlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlinesApp.swift
+//  Shared
+//
+//  Created by Damiaan on 17/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct OutlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
